a4cb5b80636c2782acb5dbb8e35a768c3f167319
HkmtArmagan <101881520+HkmtArmagan@users.noreply.github.com>
2022-04-22 16:31:22 +0300

    Update Test_01.java

--- src/githubTest/Test_01.java
+++ src/githubTest/Test_01.java
@@ -4,5 +4,6 @@ public class Test_01 {
     public static void main(String[] args) {
         System.out.println("Hala Java");
         System.out.println("Hello World");
+        System.out.println("Merhaba Github");
     }
 }
