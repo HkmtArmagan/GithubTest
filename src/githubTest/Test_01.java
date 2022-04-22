00da67b6a9341c2c8c00702f6f0d226b8d992541
Hikmet Armagan Gonenli <hkmtarmagan@gmail.com>
2022-04-22 16:29:08 +0300

    22.04

--- src/githubTest/Test_01.java
+++ src/githubTest/Test_01.java
@@ -3,5 +3,6 @@ package githubTest;
 public class Test_01 {
     public static void main(String[] args) {
         System.out.println("Hala Java");
+        System.out.println("Hello World");
     }
 }
